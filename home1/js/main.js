9f8a246d5635f0841b23d24c932fd8a1270e31e3
Sasha <>
2017-11-15 21:53:22 +0200

    add

--- home1/js/main.js
+++ home1/js/main.js
@@ -3,7 +3,11 @@ var obj1 = {
     b: "поросяти",
     с: "та вовк"
 }
-var str1;
+var str1 = String();
+
+for (let i in obj1) {
+    str1 += obj1[i] + " ";
+}
 
 console.log(str1);
 
@@ -12,11 +16,22 @@ console.log(str1);
 var arr2 = ["a", 1, "b", 23, "c", 44, "d", 0];
 var obj2 = {};
 
+for (let i = 0; i < arr2.length; i = i + 2) {
+    obj2[arr2[i]] = arr2[i + 1];
+}
+
 console.log(obj2);
 
 //--------------------------
 
 var obj3 = { a: 44, b: 21, c: 0, d: 13 }
 var arr3 = [];
+var j = 0;
+
+for (let i in obj3) {
+    arr3[j] = obj3[i];
+    j++;
+}
+arr3 = arr3.sort((a, b) => a - b);
 
 console.log(arr3);
