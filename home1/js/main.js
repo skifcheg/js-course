0f4dcc4432e3be60e895ffcbf724911dd80e4643
Sasha <>
2017-11-15 20:42:39 +0200

    add files

---
+++ home1/js/main.js
@@ -0,0 +1,22 @@
+var obj1 = {
+    a: 3,
+    b: "поросяти",
+    с: "та вовк"
+}
+var str1;
+
+console.log(str1);
+
+//--------------------------
+
+var arr2 = ["a", 1, "b", 23, "c", 44, "d", 0];
+var obj2 = {};
+
+console.log(obj2);
+
+//--------------------------
+
+var obj3 = { a: 44, b: 21, c: 0, d: 13 }
+var arr3 = [];
+
+console.log(arr3);
